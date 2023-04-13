b23a9e14baf4405138887fcdff1ed0bd7e75761f
DontLikenaming <edirna21@gmail.com>
2023-04-13 12:48:41 +0900

    콜랙션 - ArrayList 제네릭

---
+++ src/main/java/dontlikenaming/C21Collection.java
@@ -0,0 +1,138 @@
+package dontlikenaming;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class C21Collection {
+    // Collection
+    // 데이터를 저장하는 자료구조와
+    // 데이터를 처리(CRUD)하는 알고리즘을
+    // 체계적으로 정리(구조화)해서
+    // 인터페이스와 클래스로 구현해 놓은 프레임워크
+
+    public static void main(String[] args) {
+        // ArrayList
+        // 동적배열의 한 종류
+        // List 인터페이스를 구현해서 만든 컬렉션 프레임워크
+        // 요소의 저장순서가 유지
+        // 중복으로 데이터를 저장할 수 있음
+        // 크기가 늘어나면 새로운 배열을 생성하고
+        // 기존의 요소들을 옮겨야 하는 복잡한 과정이 동반됨
+/*        List<String> names = new ArrayList<>();
+        //ArrayList names = new ArrayList<>();
+
+        // 데이터 추가 : add("값")
+        names.add("혜교");
+        names.add("지현");
+        names.add("수지");
+
+        // 특정 위치에 데이터 추가 : add("위치", "값")
+        names.add(1, "원영");
+
+        // 전체 조회 : enhanced for, foreach
+        for(String name : names){
+            System.out.print(name+" ");
+        }
+        System.out.print("\n");
+
+        // 특정 요소 조회 : get("위치값")
+        System.out.println(names.get(1));
+
+        // 특정 요소 변경 : set("위치값", "새로운 값")
+        names.set(1, "윤아");
+        System.out.println(names.get(1));
+
+        // 특정 요소 제거 : remove("위치값")
+        names.remove(1);
+        System.out.println(names.get(1));
+
+        // 데이터 검색
+        // 위치로 검색 : get("위치값"), indexOf("값")
+        // 값으로 검색 : foreach, contains("값")
+
+        // "지현"을 검색 1
+        for(String name : names){
+        // for(int i=0;i<names.size();i++){
+            // if(name.get(i).equals("지현")){
+            if(name.equals("지현")){
+                System.out.print("데이터 찾음");
+            }
+        }
+        System.out.print("\n");
+
+        // "지현"을 검색 2
+        if(names.contains("지현")){
+            System.out.print("데이터 찾음");
+            System.out.print("\n");
+        }
+
+        // "지현"을 검색 2
+        if(names.indexOf("지현")>-1){
+            System.out.print("데이터 찾음");
+            System.out.print("\n");
+        }
+
+        // 게임 정보 동적 배열 생성
+        List<GameInfo> games = new ArrayList<>();
+        games.add(new GameInfo("디아블로4",89600));
+        games.add(new GameInfo("젤다의 전설",74800));
+        games.add(new GameInfo("바이오하자드 4RE",67000));
+
+        // 조회
+        // GameInfo 타입은 객체형이므로
+        // toString 재정의 없이 객체 자체를 출력 시
+        // 객체의 메모리 주소가 출력
+       for(GameInfo game : games){
+            System.out.println(game);
+        }
+
+        // "젤다의 전설"의 가격을 조회
+        for(int i=0;i<games.size();i++){
+            String game = String.valueOf(games.get(i));
+            if(game.startsWith("젤다")){
+                System.out.println(game.substring(game.length()-5));
+            }
+        }
+
+        for(GameInfo game : games){
+            String value = String.valueOf(game);
+            if(value.startsWith("젤다")){
+                System.out.println(value.substring(value.length()-5));
+            }
+        }
+
+        for(GameInfo game : games){
+            String value = String.valueOf(game);
+            if(value.contains("젤다")){
+                System.out.println(value.substring(value.length()-5));
+            }
+        }
+
+        for(GameInfo game : games){
+            String value = String.valueOf(game);
+            if(value.indexOf("젤다")>-1){
+                System.out.println(value.substring(value.length()-5));
+            }
+        }*/
+
+
+
+    }
+}
+
+class GameInfo {
+    private String name;
+    private int price;
+
+    public GameInfo(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append(name).append(", ").append(price);
+        return sb.toString();
+    }
+}
---
+++ src/main/java/dontlikenaming/C22Generic.java
@@ -0,0 +1,105 @@
+package dontlikenaming;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class C22Generic {
+    // 제네릭(Generic)
+    // 데이터 유형을 일반화(통일)한다는 것을 의미
+
+    public static void main(String[] args) {
+        // 여러 유형의 데이터를 사용하는 클래스의 경우
+        // 매개변수의 유형을 Object로 선언하는 경우
+        // 경우에 따라 원래의 유형으로 변환해야하는
+        // 번거로움이 존재하고 오류가 발생할 수 있는 가능성 존재
+
+        // 동물원 객체 생성
+        // 일반적으로 배열은 동일한 유형의
+        // 자료들을 저장하는 자료구조
+        // 하지만, 배열의 유형을 Object로 선언하면
+        // 온갖 종류의 데이터들을 저장할 수 있음
+        Object[] zoo = new Object[10];
+
+        // 동물들을 동물원 우리에 넣음
+        zoo[0] = new Lion();        // upcasting
+        zoo[1] = new Tiger();
+        zoo[2] = new Zebra();
+
+        // 우리에서 동물을 하나씩 빼냄
+        // 하지만, 배열에 저장된 데이터를 빼낼때는
+        // 적절한 형변환이 필요함!
+        Lion l1 = (Lion)zoo[0];     // downcasting
+        Tiger t1 = (Tiger)zoo[1];
+        Zebra z1 = (Zebra)zoo[2];
+
+
+        // 제네릭을 이용하면 데이터의 타입을 통일시켜
+        // 컴파일 시 타입체크 오류 방지와
+        // 불필요한 형변환을 없앨 수 있음
+        // type-parameter (<>)를 이용해서 자료구조의
+        // 데이터 유형을 미리 설정해 둠
+        List<Lion> lionzoo = new ArrayList<>();
+        lionzoo.add(new Lion());
+        //lionzoo.add(new Tiger());   // 컴파일 타임 오류 발생
+
+        // 자동 형변환이 되므로 오류없이 실행됨
+        Lion l2 = lionzoo.get(0);
+
+
+        // 제네릭 타입
+        // 클래스나 메서드의 자료형을 미리 정해두고 사용하지 않고
+        // 외부에서 사용자에 의해 정해지도록 지정하는 것을 의미
+        //
+        // 실행 중 필요에 따라 매개변수 형태로 타입을 받아 사용하므로
+        // 비슷한 기능을 지원하는 코드들을 간략하게 작성할 수 있어
+        // 재사용성이 높아짐
+
+
+        // 임의의 숫자를 입력받아 더하는 메서드를 만듬
+        Adds example = new Adds();
+
+        System.out.println(example.add(3, 3));
+        System.out.println(example.add(4.5, 5.5));
+        System.out.println(example.add(5.5f, 5.5f));
+
+        System.out.println(example.add((int)5.5f, (int)5.5f));
+        System.out.println(example.add(new Double(5.2f), new Double(5.3f)));
+
+
+
+    }
+}
+
+class Lion {}
+class Tiger {}
+class Zebra {}
+
+class Adds {
+/*    public int add (int a, int b){
+        return a + b;
+    }
+
+    public double add (double a, double b){
+        return a + b;
+    }
+
+    public float add (float a, float b){
+        return a + b;
+    }*/
+
+    public <T> T add(T a, T b){
+        T sum = null;
+        if (a.getClass() == Integer.class){  // a 변수의 클래스형이 Integer일 경우
+            System.out.println("Integer add");
+            sum = (T)(Integer)((Integer)a+(Integer)b);
+        } else if (a.getClass() == Double.class){
+            System.out.println("Double add");
+            sum = (T)(Double)((Double)a+(Double)b);
+        } else if (a.getClass() == Float.class){
+            System.out.println("Float add");
+            sum = (T)(Float)((Float)a+(Float)b);
+        }
+        return sum;
+    }
+}
